a947569e52dc07dfec81338070a0381ed4b69b20
Karolis Lasys <karolis@voxfutura.space>
2025-01-23 08:26:16 +0200

    updated sequencer

--- hs010.lua
+++ hs010.lua
@@ -7,194 +7,182 @@
 --
 -- Made by @onegin
 
-
--- set engine name
-engine.name = 'HS010'
-
 -- prereqs
 local metro = require 'metro'
 local clock = require 'clock'
 local er = require 'er'
 local lattace = require 'lattice'
 local musicutil = require 'musicutil'
+local s = require 'sequins'
 local HS010 = require 'hs010/lib/Engine_HS010'
+local player
 
 -- globals
-local lat, tab, seq_a, seq_b, note_len, redraw_loop, screen_dirty;
-
--- data structures
-
--- note cell
-local Note = {}
-
-function Note:new(note_no = 60, note_len = 1, note_pat = 0, note_vel = 100, note_vel_drift = 0)
-  new_note = {
-    note_id = note_no, -- note no.
-    note_len = note_len, -- amount of repeats
-    note_pat = note_pat, -- 0 to 7 with 7 being a tie
-    note_vel = note_vel, -- default velocity
-    note_vel_drift = note_vel_drift, -- velocity drift
-    note_idx = 0, -- index of playing note within pattern
-    note_pat = {} -- final note pattern
-  }
-  self.__index = self
-  self:gen_pat()
-  return setmetatable(new_note, self)
-end
-
-function Note:change_len(new_len)
-  self.note_len = new_len
-  self:gen_pat()
-end
-
-function Note:gen_pat()
-  if note_pat == 0 then
-    self.note_pat = er.gen(0, note_len, 0)
-  elseif note_pat == 1 then
-    self.note_pat = er.gen(1, note_len, 0)
-  elseif note_pat == 2 then
-    self.note_pat = er.gen(note_len, note_len, 0)
-  elseif note_pat == 3 then
-    self.note_pat = er.gen(math.ceil(note_len / 2), note_len, 0)
-  elseif note_pat = 4 then
-    self.note_pat = er.gen(math.ceil(note_len / 3), note_len, 0)
-  elseif note_pat = 5 then
-    self.note_pat = er.gen(math.ceil(note_len / 4), note_len, 0)
-  elseif note_pat = 6 then
-    self.note_pat = er.gen(math.random(note_len), note_len, math.random(note_len))
-  elseif note_pat = 7 then -- edge case for tie
-    self.note_pat = er.gen(1, note_len, 0)
-  end
-
-  for k, v in ipairs(self.note_pat) do
-    if v then
-      local tie = note_pat == 7
-      self.note_pat[k] = Note.gen_midinote(
-        self.note_id,
-        self.note_vel + math.random(-self.note_vel_drift, self.note_vel_drift),
-        tie
-      )
-    else
-      self.note_pat[k] = Note.gen_midinote(self.note_id, 0)
-    end
-  end
-end
-
-function Note:get_note()
-  local note = self.note_pat[self.idx + 1]
-  self.idx = self.idx + 1
-  self.idx = math.fmod(self.idx, note_len)
-  return note
-end
-
-function Note:advance()
-  return self.idx == 0
-end
-
-function Note.gen_silent()
-  return Note.gen_midinote(0, 0, false)
-end
-
-function Note.gen_midinote(note_no, note_vel, tie = false)
-  return {note_no, note_vel, tie}
-end
-
-
--- sequencer note table
-local Note_Table = {}
-
-function Note_Table:new()
-  new_table = {
-    table = {},
-    split_index = 8,
-    idx_a = 0,
-    idx_b = 7,
-    tab_len = 8
-  }
-  for i = 1, new_table.tab_len do
-    new_table.table[i] = Note:new();
-  end
-  self.__index = self
-  return setmetatable(new_note, self)
-end
-
-function Note_Table:get_note(which)
-  local note;
-
-  if which == 1 then
-    if self.split_idx != 0 then
-      local supernote = self.table[self.idx_a]
-      note = supernote:get_note()
-      if supernote:advance() then
-        self.idx_a = self.idx_a + 1
-        self.idx_a = math.fmod(self.idx_a, split_index)
-      end
-    else
-      note = Note.gen_silent()
-    end
-  else then
-    if self.split_idx != 8 then
-      local supernote = self.table[self.idx_b]
-      note = supernote:get_note()
-      if supernote:advance() then
-        self.idx_b = self.idx_a + 1
-        self.idx_b = split_index + math.fmod(self.idx_b - split_index, tab_len - split_index)
-      else
-    else
-      note = Note.gen_silent()
-    end
-  end
-
-  return note;
-end
+local lat, tab, note_len, redraw_loop, screen_dirty;
+local note_table = {}
+local seq_table = {}
+local note_const = {}
+local cur_scale = {}
+local scale_name, scale_root
 
 -- sequencer logic
 function init()
   -- add engine params
   HS010.add_params()
+  nb:init()
   -- start redraw loop
   redraw_loop = metro.init(redraw, 0.1, -1)
   redraw_loop:start()
   -- init default note len
   note_len = 1/16
-  -- init seq a and seq b lattace
+  -- init lattaces
   lat = lib_lat:new()
-  -- init note table
-  tab = Note_table:new()
-  -- init seq a and seq b
-  seq_a = lat:new_sprocket{
-    action = play_note_a()
-    division = 1/4
-    enabled = true
-  }
-  seq_b = lat:new_sprocket{
-    action = play_note_b()
-    division = 1/4
-    enabled = true
-  }
+  -- init stuff
+  init_const()
+  init_tables()
+  init_seqs()
+  init_scale("Phrygian", 60)
   -- start the sequencer
   lat:start()
   screen_dirty = true
 end
 
-function play_note_a()
-  local note = tab:get_note(1)
-  play_note(note)
+function init_scale(name, root)
+    scale_name = name
+    scale_root = math.fmod(root, 12)
+    cur_scale = MusicUtil.generate_scale(root, name, 1)
 end
 
-function play_note_b()
-  local note = tab:get_note(2)
-  play_note(note)
+function init_const()
+  note_const = {};
+  note_const.note = 0
+  note_const.vel = 127
+  note_const.oct = 3
+  note_const.off_a = 0
+  note_const.off_b = 0
+  note_const.slide = false
 end
 
-function play_note(note)
-  engine.noteOn(note[0], note[1]/128)
-  clock.run(end_note, note[0], note_len)
+function init_tables()
+  note_table = {};
+  note_table.note = s{0,0,0,0,0,0,0,0}
+  note_table.vel = s{127, 80}
+  note_table.oct = s{3, 3, 4, 3}
+  note_table.off_a = s{{0, false}, {5, false}, {3, false}, {12, false}}
+  note_table.off_b = s{{0, false}}
+  note_table.gate = s{true}
+  note_table.slide = s{false}
 end
 
-function end_note(note, note_len)
-  clock.sync(note_len)
-  engine.noteOff(note);
+function init_seqs()
+  seq_table.note = lat:new_sprocket{
+    action = set_note()
+    division = 1/4
+    enabled = true
+    order = 1
+  }
+  seq_table.vel = lat:new_sprocket{
+    action = set_vel()
+    division = 1/4
+    enabled = true
+    order = 2
+  }
+  seq_table.oct = lat:new_sprocket{
+    action = set_oct()
+    division = 1/4
+    enabled = true
+    order = 3
+  }
+  seq_table.off_a = lat:new_sprocket{
+    action = set_a()
+    division = 1/4
+    enabled = true
+    order = 4
+  }
+  seq_table.off_b = lat:new_sprocket{
+    action = set_b()
+    division = 1/4
+    enabled = true
+    order = 5
+  }
+  seq_table.slide = lat:new_sprocket{
+    action = set_slide()
+    division = 1/4
+    enabled = true
+    order = 6
+  }
+  seq_table.gate = lat:new_sprocket{
+    action = play_note()
+    division = 1/4
+    enabled = true
+    order = 7
+  }
+end
+
+function set_slide()
+  note_const.slide = note_table.slide()
+end
+
+function set_note()
+  note_const.note = note_table.note()
+end
+
+function set_vel()
+  note_const.vel = note_table.vel()
+end
+
+function set_oct()
+  note_const.oct = note_table.oct() * 12
+end
+
+function set_a()
+  note_const.off_a = note_table.off_a()
+end
+
+function set_note()
+  note_const.off_b = note_table.off_b()
+end
+
+-- implement ties
+function play_note()
+  local gate = note_table.gate()
+  if gate then
+    local note = {}
+    local poly_a = note_const.off_a[2]
+    local poly_a = note_const.off_b[2]
+    note[1] = note_const.note
+    note[2] = note_const.off_a[1]
+    note[3] = note_const.off_b[1]
+    if not poly_a then
+      note[1] = note[1] + note[2]
+    else
+      format_and_play_note(note[2], velocity_dev, note_const.oct, note_const.vel)
+    end
+    if not poly_b then
+      note[1] = note[1] + note[3]
+    else
+      format_and_play_note(note[3], velocity_dev, note_const.oct, note_const.vel)
+    end
+    format_and_play_note(note[1], 0, note_const.oct, note_const.vel)
+  end
+end
+
+function format_and_play_note(note, vdev, oct, vel)
+  local add_oct = note - fnote
+  local vd = math.min(127, math.abs(math.random(-vdev, vdev) + vel))
+  local nnote = scale_quant(note, oct)
+  play_note_engine(nnote, vd, note_len)
+end
+
+function scale_quant(note)
+  local fnote = math.fmod(math.min(math.max(note, 0), 127), 12)
+  local oct = note - fnote
+  return musicutil.snap_note_to_array(fnote, cur_scale) + oct
+end
+
+function play_note_engine(note, vel, note_time)
+  player = params:lookup_param("voice_id"):get_player()
+  player:play_note(note, vel/128, note_time)
 end
 
 -- control logic
